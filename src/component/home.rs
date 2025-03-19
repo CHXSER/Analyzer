72bfd395dc5fb77145ae34aed13b1690a69caae9
CHXSER <elmoutaren@gmail.com>
2025-03-19 19:51:40 +0100

    close #1
    Working on state & routing

--- src/component/home.rs
+++ src/component/home.rs
@@ -1,15 +1,39 @@
 use dioxus::prelude::*;
+use directories::UserDirs;
+use rfd::AsyncFileDialog;
+
+use crate::HomeContext;
 
 #[component]
 pub fn Home() -> Element {
+    let pick = async move |_evt| match pick_dir().await {
+        Some(a) => {
+            consume_context::<HomeContext>().path.set(a);
+        }
+        None => consume_context::<HomeContext>().path.set("".to_string()),
+    };
+
+    //let folder_path = use_context::<HomeContext>();
+
     rsx! {
         div { id: "container",
             h1 { "Welcome!" }
             button { id: "settings-button", "⚙" }
             div { id: "content",
-                p { "Select a folder to analyze" } 
-                button { id: "file-picker", "Choose Folder" }
+                p { "Select a folder to analyze" }
+                button { onclick: pick, id: "file-picker", "Choose Folder" }
+                        //p { "{folder_path.path}" }
             }
         }
     }
+}
+
+async fn pick_dir() -> Option<String> {
+    let user_dir = UserDirs::new().unwrap();
+    let folder = AsyncFileDialog::new()
+        .set_title("Folder to analyze")
+        .set_directory(user_dir.picture_dir().unwrap())
+        .pick_folder()
+        .await;
+    folder.map(|path| String::from(path.path().to_str().unwrap()))
 }
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,41 @@ use component::home::Home;
 static MAIN_CSS: Asset = asset!("/assets/main.css");
 static FAVICON: Asset = asset!("/assets/favicon.ico");
 
-
-
 fn main() {
-    dioxus::launch(App);
+    #[cfg(feature = "desktop")]
+    fn launch_app() {
+        use dioxus::desktop::{tao, LogicalSize};
+        let window = tao::window::WindowBuilder::new()
+            .with_resizable(true)
+            .with_title("Analyzer")
+            .with_min_inner_size(LogicalSize::new(650, 700));
+        dioxus::LaunchBuilder::new()
+            .with_cfg(
+                dioxus::desktop::Config::new()
+                    .with_window(window)
+                    .with_menu(None),
+            )
+            .launch(App);
+    }
+
+    #[cfg(not(feature = "desktop"))]
+    fn launch_app() {
+        dioxus::launch(App);
+    }
+    launch_app();
 }
 
 #[component]
 fn App() -> Element {
     rsx! {
+        //use_context_provider(|| HomeContext { path: "".to_string() });
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Home {  }
-        //Router::<Route> {}
+        Home {}
     }
+}
+
+#[derive(Clone, Copy)]
+struct HomeContext {
+    path: Signal<String>,
 }
