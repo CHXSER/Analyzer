1a817bd61081a8e47b5e75c773e3ad6f8c36e340
CHXSER <elmoutaren@gmail.com>
2025-03-22 14:08:25 +0100

    Dark theme, and url encoding

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ use component::home::Home;
 use component::loading::Loading;
 use model::media::DuplicateMedia;
 
-static MAIN_CSS: Asset = asset!("/assets/main.css");
+static LIGHT_MAIN_CSS: Asset = asset!("/assets/light_main.css");
+static DARK_MAIN_CSS: Asset = asset!("/assets/dark_main.css");
 static FAVICON: Asset = asset!("/assets/favicon.ico");
 
+static THEME: GlobalSignal<DarkTheme> = Global::new(|| DarkTheme(true));
 static DUPS: GlobalSignal<Vec<DuplicateMedia>> = Global::new(Vec::new);
 
 fn main() {
@@ -41,7 +43,11 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        if THEME.read().0 {
+            document::Stylesheet { href: DARK_MAIN_CSS }
+        } else {
+            document::Stylesheet { href: LIGHT_MAIN_CSS }
+        }
         Router::<Route> {}
     }
 }
@@ -57,3 +63,5 @@ pub enum Route {
     #[route("/comparison")]
     Comparison,
 }
+
+struct DarkTheme(bool);
