7d772c054448875e049e189323ad61d97a337241
CHXSER <elmoutaren@gmail.com>
2025-03-28 22:54:50 +0100

    close #7

--- src/component/home.rs
+++ src/component/home.rs
@@ -1,26 +1,9 @@
 use dioxus::prelude::*;
-use directories::UserDirs;
-use rfd::AsyncFileDialog;
-use std::path::PathBuf;
 
 use crate::Route;
 
 #[component]
 pub fn Home() -> Element {
-    let folder = use_signal(|| "".to_string());
-    let pick = move |_evt| {
-        let mut folder = folder.to_owned();
-        spawn(async move {
-            match pick_dir().await {
-                Some(a) => {
-                    folder.set(a.clone());
-                    let _ = use_navigator().push(Route::Loading { folder_path: a });
-                }
-                None => folder.set("".to_string()),
-            }
-        });
-    };
-
     rsx! {
         div { id: "container",
             h1 { "Welcome!" }
@@ -40,22 +23,29 @@ pub fn Home() -> Element {
             }
             div { id: "content",
                 p { "Select a folder to analyze" }
-                button { onclick: pick, id: "file-picker", "OPEN" }
+                label { class: "file-input-button",
+                    "OPEN"
+                    input {
+                        r#type: "file",
+                        class: "file-input-hidden",
+                        directory: true,
+                        onchange: move |evt| {
+                            async move {
+                                if let Some(file_engine) = evt.files() {
+                                    let files = file_engine.files();
+                                    for file_name in files {
+                                        use_navigator()
+                                            .push(Route::Loading {
+                                                folder_path: file_name,
+                                            });
+                                    }
+                                }
+                            }
+                        },
+                        "OPEN"
+                    }
+                }
             }
         }
     }
-}
-
-async fn pick_dir() -> Option<String> {
-    let user_dir = UserDirs::new().unwrap();
-    let folder = AsyncFileDialog::new()
-        .set_title("Folder to analyze")
-        .set_directory(
-            user_dir
-                .picture_dir()
-                .unwrap_or(PathBuf::from("/").as_path()),
-        )
-        .pick_folder()
-        .await;
-    folder.map(|path| String::from(path.path().to_str().unwrap()))
 }
